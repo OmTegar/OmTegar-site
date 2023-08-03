911bbc965653fe859eaf5c13772c7f7019f3973c
TEGAR_71105 <tegardito02@gmail.com>
2023-08-03 23:05:57 +0700

    build CDN Chacing Static data

--- src/pages/Certificate.jsx
+++ src/pages/Certificate.jsx
@@ -297,7 +297,7 @@ const Card = ({ data }) => {
                       <div>
                         <div className="flex items-center">
                           <a
-                            href={`https://omtegar.me/certificate/${data.link}`}
+                            href={`https://omtegar-site.sgp1.cdn.digitaloceanspaces.com/certificate/${data.link}`}
                             target="_blank"
                             className="text-white/80"
                             rel="noreferrer"
